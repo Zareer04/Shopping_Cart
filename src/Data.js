0d4d61b511f8933df625a298425d628f9ab55823
Zareer <kzareer04@gmail.com>
2025-01-12 18:08:01 +0600

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+  {
+    id: "p1",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Cotton, Casual",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "p2",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Cotton, Formal",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "p3",
+    name: "T Shirt",
+    price: 25,
+    desc: "Cotton, White",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "p4",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Cotton, Black",
+    img: "images/img-4.jpg",
+  },
+  {
+    id: "p5",
+    name: "Socks",
+    price: 5,
+    desc: "Wool, Grey",
+    img: "images/img-5.jpg",
+  },
+  {
+    id: "p6",
+    name: "Mens Beanie",
+    price: 5,
+    desc: "Wool, Black",
+    img: "images/img-6.jpg",
+  },
+  {
+    id: "p7",
+    name: "Mens Denim",
+    price: 120,
+    desc: "Jeans, Blue",
+    img: "images/img-7.jpg",
+  },
+  {
+    id: "p8",
+    name: "Mens Formal Pant",
+    price: 180,
+    desc: "Cotton, Blue",
+    img: "images/img-8.jpg",
+  },
+]
